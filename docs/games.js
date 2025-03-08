9b892c47a1b45726c1ccf11cae026480c9b6e537
srtmd <46230056+srtmd@users.noreply.github.com>
2025-03-08 14:15:11 -0300

    new

---
+++ docs/games.js
@@ -0,0 +1,21 @@
+const data = [{
+        name: "Lethal Company",
+        image: "https://cdn2.steamgriddb.com/icon/dead87a4c00de43486ca1c34d9b33c3c/32/512x512.png",
+        link: "https://lvl.nxty.cc/lc.html"
+    },
+    {
+        name: "Chaos Express: Delivery Simulator",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/2818480/library_600x900_2x.jpg?t=1738685418",
+        link: "https://lvl.nxty.cc/chaosexpress.html"
+    },
+    {
+        name: "R.E.P.O.",
+        image: "https://cdn2.steamgriddb.com/icon_thumb/3dfbf3b3c53c9a8744f6d0572b9158cf.png",
+        link: "https://lvl.nxty.cc/repo.html"
+    },
+    {
+        name: "House Flipper 2",
+        image: "https://cdn2.steamgriddb.com/icon/5761547e8b52b26d2f7f9bdc41e0336a/32/512x512.png",
+        link: "https://lvl.nxty.cc/houseflipper2.html"
+    }
+];
